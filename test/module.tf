c6aabb35dea4bdef74009c84c01cc8cd21b32bb9
kpskohli <35044385+kpskohli@users.noreply.github.com>
2022-04-18 23:35:58 -0500

    remove test

--- test/module.tf
+++
@@ -1,8 +0,0 @@
-module "aws-s3-bucket" {
-  source                = "../"
-  bucket_name           = "test-kanwar"
-  tags = {
-    Name                = "Test Bucket"
-    Environment          = "Dev"
-  }
-}
